7cc33278ead09672b60972c2e021319a529aa7c0
Martin Goff <mwgoff24@students.bmchs.org>
2022-08-18 08:13:08 -0500

    added lab 0.01 file

---
+++ Lab0.01.py
@@ -0,0 +1,16 @@
+'''
+############
+# Lab 0.01 #
+############
+
+In this lab we will create a class that will represent colors 
+and build a function to combine two colors.
+
+
+1.  Create a class, Color.
+2.  Instantiate at least 3 colors.
+3.  Add attributes of r, g, and b to those instances.
+4.  Create a function, add_color, which takes in two colors 
+    and returns a color that is the sum of the two reds, greens, 
+    and blues. Don't forget: the maximum value for R, G, or B is 255.
+'''
