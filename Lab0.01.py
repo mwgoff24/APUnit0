18c329dee825f0ca64f893841dd17ec395914e05
Martin Goff <mwgoff24@students.bmchs.org>
2022-08-18 08:54:45 -0500

    worked

--- Lab0.01.py
+++ Lab0.01.py
@@ -14,3 +14,54 @@ and build a function to combine two colors.
     and returns a color that is the sum of the two reds, greens, 
     and blues. Don't forget: the maximum value for R, G, or B is 255.
 '''
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# class Color
+class Color():
+    '''this creates different colors'''
+
+# instantiate 3 color objects
+color1 = Color()
+color2 = Color()
+color3 = Color()
+
+# set color attributes
+
+# color 1
+color1.r = 100
+color1.g = 95
+color1.b = 201
+
+# color 2
+color2.r = 23
+color2.g = 137
+color2.b = 1
+
+# color 3
+color3.r = 255
+color3.g = 55
+color3.b = 62
+
+# add color, 
+def add_color(color_a, color_b):
+    # intantiate a new color
+    new_color = Color
+
+    # assign red for new color object
+    new_color.r = color_a.r + color_b.r
+    if new_color.r > 255:
+        new_color.r = 255
+
+    # assign red for new color object
+    new_color.r = color_a.r + color_b.r
+    if new_color.r > 255:
+        new_color.r = 255
+    
+
+    return new_color
+
+color4 = add_color(color1, color2)
+
+print(f"Color 4's red attribute is {color4.r}.")
