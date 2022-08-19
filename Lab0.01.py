a0a2a60b96954234c14b5d87d4b095050f0729ce
Martin Goff <mwgoff24@students.bmchs.org>
2022-08-19 08:21:58 -0500

    finished lab 0.01

--- Lab0.01.py
+++ Lab0.01.py
@@ -14,6 +14,7 @@ and build a function to combine two colors.
     and returns a color that is the sum of the two reds, greens, 
     and blues. Don't forget: the maximum value for R, G, or B is 255.
 '''
+# max of each rgb value
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -44,10 +45,10 @@ color3.r = 255
 color3.g = 55
 color3.b = 62
 
-# add color, adds new rgb to color
+# add color, adds rgb attributes to color
 def add_color(color_a, color_b):
     # intantiate a new color
-    new_color = Color
+    new_color = Color()
 
     # assign red for new color object
     new_color.r = color_a.r + color_b.r
@@ -56,21 +57,22 @@ def add_color(color_a, color_b):
 
     # assign green for new color object
     new_color.g = color_a.g + color_b.g
-    if new_color.r > 255:
-        new_color.r = 255
+    if new_color.g > 255:
+        new_color.g = 255
     
     # assign blue for new color object
     new_color.b = color_a.b + color_b.b
-    if new_color.r > 255:
-        new_color.r = 255
+    if new_color.b > 255:
+        new_color.b = 255
 
     return new_color
 
 # making three new colors
 color4 = add_color(color1, color2)
-color5 = add_color(color2, color3)
-color6 = add_color(color1, color3)
+print(f"Color 4's RGB attributes are ({color4.r}, {color4.g}, {color4.b}). \n")
 
-print(f"Color 4's rgb attributes are ({color4.r}, {color4.g}, {color4.b}). \n")
+color5 = add_color(color2, color3)
 print(f"Color 5's rgb attributes are ({color5.r}, {color5.g}, {color5.b}). \n")
-print(f"Color 6's rgb attributes are ({color6.r}, {color6.g}, {color6.b}).")
+
+color6 = add_color(color1, color3)
+print(f"Color 6's rgb attributes are ({color6.r}, {color6.g}, {color6.b}). \n")
