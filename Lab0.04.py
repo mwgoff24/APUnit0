bb98492e06bb25a98e7609bd4453b7964557562e
Martin Goff <mwgoff24@students.bmchs.org>
2022-08-29 08:53:25 -0500

    almost finished lab 0.04

--- Lab0.04.py
+++ Lab0.04.py
@@ -42,15 +42,66 @@ my_pet = Pet()
 isinstance(my_pet, Pet) # returns true
 isinstance(my_pet, Dog) # returns false
 '''
-
+# main pokemon class
 class Pokemon():
-    pass
+    def __init__(self, name):
+        self.name = name
+
 
 class Water(Pokemon):
-    pass
+    type = 'water'
+    growl = 'Splish Splish'
+
 
 class Fire(Pokemon):
-    pass
+    type = 'fire'
+    growl = 'Fire Fire'
+
 
 class Grass(Pokemon):
-    pass
+    type = 'grass'
+    growl = 'Cheep Cheep'
+
+
+def attack(self, other):
+    # water to fire
+    if isinstance(self, Water) and isinstance(other, Fire):
+        print(f"{self.name}'s attack is more effective against {other.name}.")
+
+    # fire to grass
+    elif isinstance(self, Fire) and isinstance(other, Grass):
+        print(f"{self.name}'s attack is more effective against {other.name}.")
+
+    # grass to water
+    elif isinstance(self, Grass) and isinstance(other, Water):
+        print(f"{self.name}'s attack is more effective against {other.name}.")
+
+    # water to grass
+    elif isinstance(self, Water) and (other, Grass):
+        print(f"{self.name}'s attack is less effective against {other.name}.")
+
+    # fire to water
+    elif isinstance(self, Fire) and (other, Water):
+        print(f"{self.name}'s attack is less effective against {other.name}.")
+
+    # grass to fire
+    elif isinstance(self, Grass) and (other, Fire):
+        print(f"{self.name}'s attack is less effective against {other.name}.")
+
+
+
+my_pokemon1 = Water('Squirtle')
+my_pokemon2 = Fire('Charizard')
+my_pokemon3 = Grass('Weedy')
+
+attack(my_pokemon1, my_pokemon2)
+
+attack(my_pokemon1, my_pokemon3)
+
+attack(my_pokemon2, my_pokemon1)
+
+attack(my_pokemon2, my_pokemon3)
+
+attack(my_pokemon3, my_pokemon2)
+
+attack(my_pokemon3, my_pokemon1)
