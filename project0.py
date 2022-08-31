716a0c7304c4630d123d5b91b64c9d427bb825ce
Martin Goff <mwgoff24@students.bmchs.org>
2022-08-31 09:18:16 -0500

    worked on game loop

--- project0.py
+++ project0.py
@@ -3,35 +3,15 @@
 
 import random
 
-# create user class for player and user
-class User:
-    def __init__(self, name):
-        self.name = name
-        self.pokemon = []
-        self.current_pokemon = None
-
-    def switch(self):
-        pass
-
-    def attack(self, target, attack_name):
-        pass
-
-    def heal(self):
-        pass
-
-    def print_choices(self):
-        pass
-
-class Computer(User):
-    pass
-
-
 # main pokemon class
 class Pokemon():
     def __init__(self, name, hp, ap):
         self.name = name
         self.hp = hp
         self.ap = ap
+
+    def __str__(self):
+        return f"{self.name}: {self.hp} HP, {self.ap} AP"
 
 
 # three child classes of pokemon
@@ -66,6 +46,40 @@ class Grass(Pokemon):
         "Razor Leaf": [55, 95]
               }
 
+# create user class for player and computer
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.pokemon = []
+        self.current_pokemon = None
+
+    def switch(self):
+        self.new_pokemon = input("Which of your pokemon would you like to switch to? ")
+        self.current_pokemon = self.new_pokemon
+
+    def attack(self, target, attack_name):
+        pass
+
+    def heal(self):
+        self.current_pokemon
+
+    def print_choices(self, poke_list):
+        print(f"{self.name}, Here are your available pokemon:")
+        num = 1
+        for item in user_poke_list:
+            print(f"{item}, {num}")
+            num += 1
+
+    def poke_choices(self, poke_list):
+        poke_choice = int(input(f"Select a pokemon {self.name}. ")) - 1
+        player.pokemon.append(poke_list[poke_choice])
+        user_poke_list.remove(poke_list[poke_choice])
+
+class Computer(User):
+    pass
+
+
+
 w_poke1 = Water('Squirtle', 80, 20)
 w_poke2 = Water('Psyduck', 70, 40)
 w_poke3 = Water('Pollywag', 50, 50)
@@ -76,4 +90,23 @@ g_poke1 = Grass('Bulbasoar', 60, 40)
 g_poke2 = Grass('Bellsprout', 40, 60)
 g_poke3 = Grass('Oddish', 50, 50)
 
-print(w_poke3.attacks)
+user_poke_list = [w_poke1, w_poke2, w_poke3, f_poke1, f_poke2, f_poke3, g_poke1, g_poke2, g_poke3]
+computer_poke_list = [w_poke1, w_poke2, w_poke3, f_poke1, f_poke2, f_poke3, g_poke1, g_poke2, g_poke3]
+
+# game loop
+
+# setting names
+user_name = input("Welcome player! What is your name? ")
+computer_name = input("What do you want the computer's name to be? ")
+
+# instantiate User objects
+player = User(user_name)
+computer = Computer(computer_name)
+
+# player chooses pokemon
+player.print_choices(user_poke_list)
+player.poke_choices(user_poke_list)
+
+
+print(player.pokemon)
+print(user_poke_list)
