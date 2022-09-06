d1bd77649f067e49ecfb413f77fda709a3d4b8fd
Martin Goff <mwgoff24@students.bmchs.org>
2022-09-06 08:53:01 -0500

    finished pre game loop

--- project0.py
+++ project0.py
@@ -14,6 +14,7 @@ class Pokemon():
         return f"{self.name}: {self.hp} HP, {self.ap} AP"
 
 
+
 # three child classes of pokemon
 class Water(Pokemon):
     type = 'water'
@@ -46,6 +47,15 @@ class Grass(Pokemon):
         "Razor Leaf": [55, 95]
               }
 
+
+
+
+
+
+
+
+
+
 # create user class for player and computer
 class User:
     def __init__(self, name):
@@ -53,13 +63,16 @@ class User:
         self.pokemon = []
         self.current_pokemon = None
 
+# switches pokemon to another
     def switch(self):
         self.new_pokemon = input("Which of your pokemon would you like to switch to? ")
         self.current_pokemon = self.new_pokemon
 
+# attacks the computer's pokemon
     def attack(self, target, attack_name):
         pass
 
+# heals current pokemon
     def heal(self):
         self.current_pokemon
 
@@ -79,22 +92,29 @@ class User:
         player.print_choices(poke_list)
 
 # used for debugging purposes
-    def print_personal(self, poke_list):
+    def print_current(self, poke_list):
         for item in poke_list:
             print(item)
 
+# sets a pokemon chosen as first to fight with
     def set_current_pokemon(self, poke_list):
+        player.print_choices(poke_list)
         current = int(input("Which of your pokemon would you like to use? ")) - 1
-        self.current = poke_list[current]
+        self.current_pokemon = poke_list[current]
+        poke_list.remove(self.current_pokemon)
+
 
 
 class Computer(User):
+# same as User's poke_choices but implemented for computer
     def computer_choices(self, poke_list):
         new_pokemon = random.randint(0, int(len(poke_list))-1)
         computer.pokemon.append(poke_list[new_pokemon])
         computer_poke_list.remove(poke_list[new_pokemon])
 
-
+# same as User set_current_pokemon but implemented for computer
+    def computer_current(self, poke_list):
+        computer.current = random.randint(0, int(len(poke_list))-1)
 
 
 w_poke1 = Water('Squirtle', 80, 20)
@@ -110,7 +130,15 @@ g_poke3 = Grass('Oddish', 50, 50)
 user_poke_list = [w_poke1, w_poke2, w_poke3, f_poke1, f_poke2, f_poke3, g_poke1, g_poke2, g_poke3]
 computer_poke_list = [w_poke1, w_poke2, w_poke3, f_poke1, f_poke2, f_poke3, g_poke1, g_poke2, g_poke3]
 
-# game loop
+
+
+
+
+
+
+
+
+# pre game loop
 
 # setting names
 user_name = input("Welcome player! What is your name? ")
@@ -132,7 +160,32 @@ while len(computer.pokemon) < 3:
 
 # game tells player what pokemon the computer chose
 print(f"\n{computer.name} has now chosen their pokemon:")
-computer.print_personal(computer.pokemon)
+computer.print_current(computer.pokemon)
+print("\n")
 
+# set player's current pokemon
+player.set_current_pokemon(player.pokemon)
+
+# set computer's current pokemon
+computer.computer_current(computer.pokemon)
+
+# game loop
 while True:
-    pass
+    # defining some variables
+    player1 = player
+    player2 = computer
+    turn = player1
+
+    # asks player what move they want to do
+    move = input(f"{turn.name}, what would you like to do? s for switch, a for attack, or h for heal. ")
+    if move == 's':
+        player.switch()
+    elif move == 'a':
+        player.attack()
+    elif move == 'h':
+        player.heal()
+    else:
+        print("That is not an available option.")
+        move
+    # turn moved to computer
+    turn = player2
